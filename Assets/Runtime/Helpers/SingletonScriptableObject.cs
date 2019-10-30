235ca56964de47c83fe1e78f519ca5b4721a8b38
railek <jean@railek.com>
2019-10-30 16:00:13 +0200

    feat: reload-proof singletons from scriptableobjects

---
+++ Assets/Runtime/Helpers/SingletonScriptableObject.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using Railek.Unibase.Utilities;
+using UnityEngine;
+
+namespace Railek.Unibase.Helpers
+{
+    public abstract class SingletonScriptableObject<T> : ScriptableObject where T : ScriptableObject
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance != null)
+                {
+                    return _instance;
+                }
+
+                var typeName = typeof(T).Name;
+                var classPath = $"{Application.dataPath}/Resources/{typeName}.asset";
+
+                if (!File.Exists(classPath))
+                {
+                    var createInstance = Asset.Create<T>("Assets/Resources/");
+                    _instance = createInstance;
+                    return createInstance;
+                }
+
+                var loadInstance = (T) Resources.Load(typeName, typeof(T));
+                _instance = loadInstance;
+                return loadInstance;
+            }
+        }
+    }
+}
---
+++ Assets/Runtime/Utilities/Asset.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Railek.Unibase.Utilities
+{
+    public static class Asset
+    {
+        public static T Create<T>(string relativePath) where T : ScriptableObject
+        {
+            if (!Directory.Exists(relativePath))
+            {
+                Directory.CreateDirectory(relativePath);
+            }
+
+            if (string.IsNullOrEmpty(relativePath))
+            {
+                return null;
+            }
+
+            var asset = ScriptableObject.CreateInstance<T>();
+            var typeName = typeof(T).Name;
+
+            AssetDatabase.CreateAsset(asset, relativePath + typeName + ".asset");
+            EditorUtility.SetDirty(asset);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            return asset;
+        }
+    }
+}
