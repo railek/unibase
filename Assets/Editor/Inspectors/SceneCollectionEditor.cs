38cb9eff1d738f636fb7ae77219e14b2fb94cf6e
railek <jean@railek.com>
2019-11-29 09:37:26 +0200

    feat: saving and loading scene configurations using scriptableObjects

---
+++ Assets/Editor/Inspectors/SceneCollectionEditor.cs
@@ -0,0 +1,61 @@
+﻿using Railek.Unibase.Utilities;
+using UnityEditor;
+using UnityEngine;
+
+namespace Railek.Unibase.Editor
+{
+    [CustomEditor(typeof(SceneCollection))]
+    public class SceneCollectionEditor : EditorBase
+    {
+        [MenuItem("Scene/Save Scene Setup")]
+        public static void SaveSetup()
+        {
+            var setupFile = Asset.Create<SceneCollection>("Assets/Resources/");
+            setupFile.SaveSetup();
+        }
+
+        private SerializedProperty _setupProperty;
+
+        protected override void OnEnable()
+        {
+            _setupProperty = GetProperty("setup");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var setup = target as SceneCollection;
+
+            EditorGUILayout.PropertyField(_setupProperty, true);
+
+            GUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Save Current Setup"))
+            {
+                if (setup != null)
+                {
+                    setup.SaveSetup();
+                }
+            }
+
+            if (GUILayout.Button("Load"))
+            {
+                if (setup != null)
+                {
+                    setup.LoadSetup();
+                }
+            }
+
+            if (GUILayout.Button("Load (Inclusive)"))
+            {
+                if (setup != null)
+                {
+                    setup.LoadSetupInclusive();
+                }
+            }
+
+            GUILayout.EndHorizontal();
+        }
+    }
+}
