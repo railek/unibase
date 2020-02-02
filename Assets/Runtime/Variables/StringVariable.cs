94e9c38354c7c831ef6ce8b05327c1d88e583e09
railek <jean@railek.com>
2020-02-02 22:44:47 +0200

    refactor: remove variables & references

--- Assets/Runtime/Variables/StringVariable.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Railek.Unibase
-{
-    [CreateAssetMenu(fileName = "StringVariable.asset", menuName = "Variables/string")]
-    public sealed class StringVariable : Variable<string>
-    {
-    }
-}
